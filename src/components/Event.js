cce411fcdefb4fca566125c6962d3ddcb3b1123c
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-07-20 12:58:06 -0400

    added mockdata to be displayed in the dom

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,3 +1,28 @@
-export const Event = () => {
-  return <li></li>;
+import { useState } from 'react';
+import mockData from '../mock-date';
+
+const Event = () => {
+  const [showDetails, setShowDetails] = useState(false);
+  const event = mockData;
+
+  const toggleDetails = () => {
+    setShowDetails(!showDetails);
+  };
+
+  return (
+    <>
+      <li>
+        <p>{event[0].location}</p>
+        <p>{event[0].summary}</p>
+        <p>{event[0].created}</p>
+        {/* <p>{event[0].description}</p> */}
+        {showDetails ? <p>{event[0].description}</p> : null}
+      </li>
+      <button onClick={toggleDetails} className='show-details'>
+        Show Details
+      </button>
+    </>
+  );
 };
+
+export default Event;
