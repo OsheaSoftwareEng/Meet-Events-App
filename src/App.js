500c981cc6172c605882f1bf89e1876258d2ece3
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-07-21 13:26:18 -0400

    updated an added features to dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 import EventList from './components/EventList';
 import CitySearch from './components/CitySearch';
 import NumberOfEvents from './components/NumberOfEvents';
+import { extractLocations, getEvents } from './api';
 
 const App = () => {
+  const [allLocations, setAllLocations] = useState([]);
+  const [events, setEvents] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const allEvents = await getEvents();
+      setEvents(allEvents);
+      setAllLocations(extractLocations);
+    };
+    fetchData();
+  }, []);
+
   return (
     <div className='App'>
-      <EventList />
-      <CitySearch />
+      <EventList events={events} />
+      <CitySearch allLocations={allLocations} />
       <NumberOfEvents />
     </div>
   );
