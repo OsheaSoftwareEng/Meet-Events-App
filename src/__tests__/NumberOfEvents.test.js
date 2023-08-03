20a9e613309dcf508accd1cd3316a8418180d841
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-08-03 13:39:40 -0400

    added integration to make sure events change on dom when user inputs a different number

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,11 +1,12 @@
 import NumberOfEvents from '../components/NumberOfEvents';
-import { render } from '@testing-library/react';
+import { render, within, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
+import App from '../App';
 
 describe('<NumberOfEvents /> component', () => {
   let EventNumber;
   beforeEach(() => {
-    EventNumber = render(<NumberOfEvents />);
+    EventNumber = render(<NumberOfEvents setCurrentNOE={() => {}} />);
   });
 
   test('checking to see if the textbox is rendering correctly', () => {
@@ -21,5 +22,24 @@ describe('<NumberOfEvents /> component', () => {
 
     await userEvent.type(textBox, '{backspace}{backspace}10');
     expect(EventNumber.queryByRole('textbox')).toHaveValue('10');
+  });
+});
+
+describe('<NumberOfEvents /> integration', () => {
+  test('renders suggestions list when the app is rendered.', async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+    const numberOfEventsDOM = AppDOM.querySelector('#event-number');
+    const numberTextBox = within(numberOfEventsDOM).queryByRole('textbox');
+    await user.click(numberTextBox);
+    await userEvent.type(numberTextBox, '{backspace}{backspace}10');
+
+    const EventListDOM = AppDOM.querySelector('#event-list');
+    await waitFor(() => {
+      const EventListItems = within(EventListDOM).queryAllByRole('listitem');
+      expect(EventListItems.length).toBe(10);
+    });
   });
 });
