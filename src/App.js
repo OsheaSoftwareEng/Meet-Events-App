48490dd08fdd18da96109e57fce5f6b85e7029be
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-08-03 13:36:54 -0400

    added all components and added functions to render elements to actual ui

--- src/App.js
+++ src/App.js
@@ -8,21 +8,28 @@ import { extractLocations, getEvents } from './api';
 const App = () => {
   const [allLocations, setAllLocations] = useState([]);
   const [events, setEvents] = useState([]);
+  const [currentNOE, setCurrentNOE] = useState(32);
+  const [currentCity, setCurrentCity] = useState('See all cities');
+
+  const fetchData = async () => {
+    const allEvents = await getEvents();
+    const filteredEvents =
+      currentCity === 'See all cities'
+        ? allEvents
+        : allEvents.filter((event) => event.location === currentCity);
+    setEvents(filteredEvents.slice(0, currentNOE));
+    setAllLocations(extractLocations(allEvents));
+  };
 
   useEffect(() => {
-    const fetchData = async () => {
-      const allEvents = await getEvents();
-      setEvents(allEvents);
-      setAllLocations(extractLocations);
-    };
     fetchData();
-  }, []);
+  }, [currentCity, currentNOE]);
 
   return (
     <div className='App'>
+      <CitySearch allLocations={allLocations} setCurrentCity={setCurrentCity} />
+      <NumberOfEvents setCurrentNOE={setCurrentNOE} />
       <EventList events={events} />
-      <CitySearch allLocations={allLocations} />
-      <NumberOfEvents />
     </div>
   );
 };
