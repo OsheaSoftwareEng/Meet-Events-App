1b9289f9fd70e4ea2408376ba4772aa7f2273e8b
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-08-09 13:58:26 -0400

    updated app component and took out waitfor

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -8,25 +8,19 @@ import { getEvents } from '../api';
 describe('<App /> component', () => {
   test('renders list of events', async () => {
     render(<App />);
-    await waitFor(() => {
-      expect(screen.getByTestId('events')).toBeInTheDocument();
-    });
+    expect(screen.getByTestId('events')).toBeInTheDocument();
   });
   //test
   test('render CitySearch', async () => {
     render(<App />);
-    await waitFor(() => {
-      expect(
-        screen.getByPlaceholderText('Search for a city')
-      ).toBeInTheDocument();
-    });
+    expect(
+      screen.getByPlaceholderText('Search for a city')
+    ).toBeInTheDocument();
   });
 
   test('renders number of events component', async () => {
     render(<App />);
-    await waitFor(() => {
-      expect(screen.getByPlaceholderText('enter a number')).toBeInTheDocument();
-    });
+    expect(screen.getByPlaceholderText('enter a number')).toBeInTheDocument();
   });
 });
 
