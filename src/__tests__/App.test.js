67e28c8eb7aed652d9b018f74f3d7bec07a0c2c6
Oshea Sanders <osheasoftwareeng@gmail.com>
2023-08-21 12:33:24 -0400

    added code to pass test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -53,3 +53,20 @@ describe('<App /> integration', () => {
     });
   });
 });
+
+const { ResizeObserver } = window;
+
+beforeEach(() => {
+  //@ts-ignore
+  delete window.ResizeObserver;
+  window.ResizeObserver = jest.fn().mockImplementation(() => ({
+    observe: jest.fn(),
+    unobserve: jest.fn(),
+    disconnect: jest.fn()
+  }));
+});
+
+afterEach(() => {
+  window.ResizeObserver = ResizeObserver;
+  jest.restoreAllMocks();
+});
